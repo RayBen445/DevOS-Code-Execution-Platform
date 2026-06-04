fe72c8d2bcad1dab6811fb9be4e8dec3f8a7db9f
Oladoye Heritage <oladoyeheritage445@gmail.com>
2026-06-04 22:28:32 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -90,7 +90,7 @@ if (trustProxySetting === "true") {
 } else {
   app.set("trust proxy", 1);
 }
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 // ---------------------------------------------------------------------------
 // GitHub App helpers
